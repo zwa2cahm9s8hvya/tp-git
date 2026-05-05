d36d136e08c17d19c6424e05d13ade8091173155
Yohan <yohanpeyrot@gmail.com>
2026-05-05 16:23:20 +0200

    test: restore passing assertion

--- test/math.test.js
+++ test/math.test.js
@@ -4,7 +4,7 @@ const assert = require("node:assert/strict");
 const { add, divide, multiply } = require("../src/math");
 
 test("add returns the sum of two numbers", () => {
-  assert.equal(add(2, 3), 999);
+  assert.equal(add(2, 3), 5);
 });
 
 test("multiply returns the product of two numbers", () => {
